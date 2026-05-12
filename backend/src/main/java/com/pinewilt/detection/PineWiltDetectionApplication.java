6f3261e467ed72aee483d4f0905ee28aec625d8f
liumengyue1234 <154077873+liumengyue1234@users.noreply.github.com>
2026-05-12 20:00:34 +0800

    feat: add backend/src/main/java/com/pinewilt/detection/PineWiltDetectionApplication.java

---
+++ backend/src/main/java/com/pinewilt/detection/PineWiltDetectionApplication.java
@@ -0,0 +1,11 @@
+package com.pinewilt.detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PineWiltDetectionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PineWiltDetectionApplication.class, args);
+    }
+}
