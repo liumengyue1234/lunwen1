c0361dde6809bd58db3352a686d7c71f59457d99
liumengyue1234 <154077873+liumengyue1234@users.noreply.github.com>
2026-05-12 20:01:08 +0800

    feat: add backend/src/main/java/com/pinewilt/detection/mapper/DetectionTaskMapper.java

---
+++ backend/src/main/java/com/pinewilt/detection/mapper/DetectionTaskMapper.java
@@ -0,0 +1,8 @@
+package com.pinewilt.detection.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pinewilt.detection.entity.DetectionTask;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DetectionTaskMapper extends BaseMapper<DetectionTask> {}
